b1b3603b9ed2a850adade1c4b4b500aeb3b80b0c
Guilherme Dias <gui.diasz@hotmail.com>
2021-02-22 14:44:45 -0300

    ajustes no header + ajustes no footer + sessão equipe

--- assets/js/cabecalho.js
+++ assets/js/cabecalho.js
@@ -1,6 +1,17 @@
-const navToggle = document.querySelector(".cabecalho__hamburger");
-const links = document.querySelector(".cabecalho__menu");
+const header = document.querySelector('.cabecalho');
+const hamburger = header.querySelector('.cabecalho__hamburger');
+const menu = header.querySelector('.cabecalho__menu')
+const links = header.querySelectorAll('.cabecalho__link');
 
-navToggle.addEventListener("click", function () {
-  links.classList.toggle("cabecalho__menu--ativo");
+hamburger.addEventListener('click', function () {
+  header.classList.toggle('ativo');
+  menu.classList.toggle('ativo');
 });
+
+links.forEach(link => {
+  link.addEventListener('click', function (){
+    menu.classList.toggle('ativo');
+    header.classList.toggle('ativo');
+  });  
+});
+
